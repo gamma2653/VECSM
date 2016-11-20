d0a3d357ec0b03d29ab21d0290f491d545d8e8f0
Christopher De Jesus <Christopher De Jesus>
2016-11-19 22:50:22 -0500

    Fixed Main Import class

--- src/main/java/com/gamsion/chris/Main.java
+++ src/main/java/com/gamsion/chris/Main.java
@@ -1,15 +1,25 @@
 package com.gamsion.chris;
+
 import static spark.Spark.*;
 import java.sql.*;
+
 public class Main {
-	public static void main(String[] args){
-		try{
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-		}catch(ClassNotFoundException e){
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.amazon.redshift.jdbc41.Driver");
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
+		try {
+			Driver myDriver = new oracle.jdbc.driver.OracleDriver();
+			DriverManager.registerDriver(myDriver);
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Error: unable to load driver class!");
+			System.exit(1);
+		}
+
 		get("/hello", (request, response) -> "Hello World!");
-		
+
 	}
 }
